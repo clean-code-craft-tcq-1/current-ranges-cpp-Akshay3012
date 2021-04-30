6aab9a0aa24301ad5dcc519f21ec76afa40cadcd
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-04-30 17:22:29 +0530

    Update current_ranges_test.cpp

--- current_ranges_test.cpp
+++ current_ranges_test.cpp
@@ -9,7 +9,7 @@ int main ()
 {
 	// Check for No (0) Current readings
   	std::vector<CurrentReadingsParameters*> RangeAndReadings = {};
-	processCurrentReadingsDatas({}, RangeAndReadings);
+	processCurrentReadingsData({}, RangeAndReadings);
 	assert(RangeAndReadings.empty() == true);
 	
 	// Number of Ranges in the Current Readings

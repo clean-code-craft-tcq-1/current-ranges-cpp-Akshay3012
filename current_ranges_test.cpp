5189b6c0b66f6261b66bd8bde41c858188a99593
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-04-30 17:27:39 +0530

    Update current_ranges_test.cpp

--- current_ranges_test.cpp
+++ current_ranges_test.cpp
@@ -18,7 +18,6 @@ int main ()
 	
 	// Check for Minimum and Maximum values of ranges
 	processCurrentReadingsData({ 1, 2, 2, 3, 4, 24, 25, 26 }, RangeAndReadings);
-	assert(RangeAndReadings[0]->current_min == 1);
 	assert(RangeAndReadings[1]->current_max == 26);
 	
 

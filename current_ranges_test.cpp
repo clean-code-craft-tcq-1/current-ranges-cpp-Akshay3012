54850836e6d7f8454b83996719a3a6a01cf06746
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-04-30 14:11:56 +0530

    Create current_ranges_test.cpp

---
+++ current_ranges_test.cpp
@@ -0,0 +1,10 @@
+#include "current_ranges.h"
+#include <assert.h>
+#include <cmath>
+
+int main ()
+{
+  std::vector<CurrentReadingsParameters*> RangeAndReadings = {};
+	processRangeInfo({ 1, 2, 2, 3, 4, 24, 25, 26 }, RangeAndReadings);
+	assert(RangeAndReadings.size()==2);
+}

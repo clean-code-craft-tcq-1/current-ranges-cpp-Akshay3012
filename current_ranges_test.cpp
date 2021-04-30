171a735d8a5e6b8f026757dc898d8ac457b3f023
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-04-30 17:24:21 +0530

    Update current_ranges_test.cpp

--- current_ranges_test.cpp
+++ current_ranges_test.cpp
@@ -13,12 +13,10 @@ int main ()
 	assert(RangeAndReadings.empty() == true);
 	
 	// Number of Ranges in the Current Readings
-  	std::vector<CurrentReadingsParameters*> RangeAndReadings = {};
 	processCurrentReadingsData({ 1, 2, 2, 3, 4, 24, 25, 26 }, RangeAndReadings);
 	assert(RangeAndReadings.size()==2);
 	
 	// Check for Minimum and Maximum values of ranges
-  	std::vector<CurrentReadingsParameters*> RangeAndReadings = {};
 	processCurrentReadingsData({ 1, 2, 2, 3, 4, 24, 25, 26 }, RangeAndReadings);
 	assert(RangeAndReadings[0]->current_max == 4);
 	assert(RangeAndReadings[1]->current_min == 24);
